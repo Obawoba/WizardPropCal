2b337db3d911bcc66b3e9746e693c9a6f1a8497a
= <=>
2016-06-26 00:30:15 +0200

    some minor additions

--- PropCal.f90
+++ PropCal.f90
@@ -10,20 +10,41 @@ program PropCal
 		RED = EC//'[91m', GREEN = EC//'[92m', YELLOW = EC//'[93m', BLUE = EC//'[96m'
 
 	integer :: NumCards, NumPlayers, Position
-	character :: TrumpColor
+	character(1) :: TrumpColor
 
 
 	print*, "This program calculates card propabilities"
 	print*,
 
-	print*, "Number of Players"
-	read*, NumPlayers
-	print*, NumPlayers
+	do while (NumPlayers < 2 .and. NumPlayers > 7)
+		print*, "Number of Players (3 to 6)"
+		read*, NumPlayers
+		print*, NumPlayers
+	enddo
 
 	print*, "Number of Cards (aka round)"
 	read*, NumCards
-	print*, "Number of Cards is: " NumCards
+	print*, "Number of Cards is: ",NumCards
+
+	print*, "Color of trump (CANT STUMP THE TRUMP)"
+	print*, "Valid colors are: " &
+		//RED//'r '//GREEN//'g '//BLUE//'b '//YELLOW//'y '//ED//'n'
+	read*, TrumpColor
+	print*, TrumpColorCodeChar(TrumpColor)
 
 contains
-
+	character(6) function TrumpColorCodeChar(TrumpColor)
+		implicit none
+		character, INTENT(IN) :: TrumpColor
+		select case (TrumpColor)
+			case('r')
+				TrumpColorCodeChar = RED//'r'
+			case('g')
+				TrumpColorCodeChar = GREEN//'g'
+			case('b')
+				TrumpColorCodeChar = BLUE//'b'
+			case('y')
+				TrumpColorCodeChar = YELLOW//'y'
+		end select
+	end function
 end program PropCal
