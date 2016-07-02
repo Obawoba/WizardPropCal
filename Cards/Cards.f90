583eb0f71b76fe4fccc2fb459cbbbe0f0d22c95d
admin <admin>
2016-07-02 17:41:15 +0200

    some more work on cards module

--- Cards/Cards.f90
+++ Cards/Cards.f90
@@ -1,29 +1,48 @@
 module cards
   implicit none
+  private
+  public::card, getCard, trumpColor
 
   type :: card(color,value)
     character :: color
     integer :: value
   end type card
 
+  interface operator (>)
+    function takes(a,b)
+      type(card),intent(in) :: a,b
+      logical :: takes
+      !implement card comparison here!
+    end function
+  end interface operator
 
-  print*,'play a card!'
-  read*,inputChar
+  character :: trumpColor
 
-  testCard = card(inputChar)
-  select case testCard % color
-    case ('r')
 
-    case ('g')
-
-    case ('b')
-
-    case ('y')
-  end select
 contains
-  type(card) function card(inputChar)
-    character, intent(in) :: inputChar
+  type(card) function getCard(inputChar)
+    character(3), intent(in) :: inputChar
+    type(card) :: tmpcard
+    tmpcard%value=getCardValue(inputChar)
+    tmpcard%color=getCardColor(inputChar)
+  end function getCard
 
-  end function card
+  integer function getCardValue(inputChar)
+    character(3),INTENT(IN) :: inputChar
+    if(inputChar(3:)==' ') then !One digit!
+      read(inputChar(2:2),'(i4)') getCardValue
+    else                        !two digits!
+      read(inputChar(2:3),'(i4)') getCardValue
+    end if
+  end function
+
+  character function getCardColor(inputChar)
+    character(3),intent(in) :: inputChar
+    select case inputChar(1)
+    case('r','g','b','y','n','z')
+      getCardColor = inputChar(1)
+    case default
+      getCardColor = 'x'
+  end function
 
 end module cards
--- Cards/cardsTest.f90
+++ Cards/cardsTest.f90
@@ -1,7 +1,25 @@
 program cardsTest
   implicit none
 
+  use cards
+
   type(card) :: testCard
   character(3) :: inputChar
+
+  print*,'play a card!'
+  read*,inputChar
+
+  testCard = card(inputChar)
+  select case testCard % color
+    case ('r')
+      print*, 'red'
+    case ('g')
+  print*, 'green'
+    case ('b')
+  print*, 'blue'
+    case ('y')
+      print*, 'yellow'
+    case default
+  end select
 
 end program cardsTest
